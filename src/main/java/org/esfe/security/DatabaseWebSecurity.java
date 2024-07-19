466a74f91400a05efb0704511337b8032856c99e
JC Tula <jc_tula@outlook.com>
2024-07-18 20:04:25 -0600

    configuración de la protección de rutas

--- src/main/java/org/esfe/security/DatabaseWebSecurity.java
+++ src/main/java/org/esfe/security/DatabaseWebSecurity.java
@@ -2,9 +2,11 @@ package org.esfe.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -21,5 +23,19 @@ public class DatabaseWebSecurity {
                 "where u.user_name = ?");
 
         return users;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(authorize -> authorize
+                // aperturar el acceso a los recursos estáticos
+                .requestMatchers("/assets/**", "/css/**", "/js/**").permitAll()
+                // las vistas públicas no requieren autenticación
+                .requestMatchers("/", "/privacy", "/terms").permitAll()
+                // todas las demás vistas requieren autenticación
+                .anyRequest().authenticated());
+            http.formLogin(form -> form.permitAll());
+
+            return http.build();
     }
 }
