edd1dcdf6ea6f6d7a4fed0a5af175cb6427f2650
JC Tula <jc_tula@outlook.com>
2024-07-18 19:39:58 -0600

    funcionalida de login con tablas rol y usuario

--- src/main/java/org/esfe/entities/Usuario.java
+++ src/main/java/org/esfe/entities/Usuario.java
@@ -1,2 +1,99 @@
-package org.esfe.entities;public class Usuario {
+package org.esfe.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre es requerido")
+    private String name;
+
+    @NotBlank(message = "El nombre de es requerido")
+    private String userName;
+
+    @NotBlank(message = "La clave de usuario es requerida")
+    private String password;
+
+    @NotBlank(message = "El corre es requerido")
+    private String email;
+
+    private int status;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "usuario_rol",
+                joinColumns = @JoinColumn(name = "usuario_id"),
+                inverseJoinColumns = @JoinColumn(name = "rol_id"))
+    private List<Rol> roles;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public List<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Rol> roles) {
+        this.roles = roles;
+    }
+
+    // Método para agregar roles
+    public void agregar(Rol tempRol) {
+        if (roles == null) {
+            roles = new LinkedList<>();
+        }
+        roles.add(tempRol);
+    }
 }
--- src/main/java/org/esfe/security/DatabaseWebSecurity.java
+++ src/main/java/org/esfe/security/DatabaseWebSecurity.java
@@ -1,2 +1,25 @@
-package org.esfe.security;public class DatabaseWebSecurity {
+package org.esfe.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class DatabaseWebSecurity {
+    @Bean
+    public UserDetailsManager customUsers(DataSource dataSource){
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.setUsersByUsernameQuery("select user_name, password, status from usuarios where user_name = ?");
+        users.setAuthoritiesByUsernameQuery("select u.user_name, r.nombre from usuario_rol ur " +
+                "inner join usuarios u on u.id = ur.usuario_id " +
+                "inner join roles r on r.id = ur.rol_id " +
+                "where u.user_name = ?");
+
+        return users;
+    }
 }
